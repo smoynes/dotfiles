58ff087ea12b35de66261b86699cc8d60b178d3d
Scott Moynes <smoynes@Graham-2.local>
2014-02-25 10:29:38 -0500

    Crustimize.

---
+++ smoynes.el
@@ -0,0 +1,16 @@
+(starter-kit-load "python")
+
+(tool-bar-mode nil)
+(column-number-mode t)
+
+(starter-kit-install-if-needed 'golden-ratio)
+(require 'golden-ratio)
+(golden-ratio-mode)
+
+(starter-kit-install-if-needed 'php-mode)
+
+(starter-kit-install-if-needed 'magit)
+(global-set-key (kbd "C-c g") 'magit-status)
+
+(starter-kit-install-if-needed 'ace-jump-mode)
+(global-set-key (kbd "C-c j") 'ace-jump-word-mode)
